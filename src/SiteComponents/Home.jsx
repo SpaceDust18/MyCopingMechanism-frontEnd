885148b84cb486e47dbe740a7ca28c7c196ac0ef
Claudia <cludama83@gmail.com>
2025-10-21 23:11:25 -0400

    refactor(ui): - move Quotes and Prompts sections below Hero component on Home page - improve homepage content flow for better user experience

--- src/SiteComponents/Home.jsx
+++ src/SiteComponents/Home.jsx
@@ -1,12 +1,12 @@
 // src/SiteComponents/Home.jsx
 import { useLayoutEffect } from "react";
 import Hero from "./Sections/Hero";
+import Quote from "./Sections/Quote.jsx";
+import Reflections from "./Sections/Reflections.jsx";
 import Blog from "./Sections/Blog.jsx";
 import MindBody from "./Sections/MindBody.jsx";
 import Nutrition from "./Sections/Nutrition.jsx";
 import OTThings from "./Sections/OTthings.jsx";
-import Quote from "./Sections/Quote.jsx";
-import Reflections from "./Sections/Reflections.jsx";
 import Contact from "./Forms/Contact.jsx";
 
 export default function Home() {
@@ -18,13 +18,13 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <Quote />
+      {/* Prevent reflections from auto-scrolling the window on Home */}
+      <Reflections autoScroll={false} />
       <Blog />
       <MindBody />
       <Nutrition />
       <OTThings />
-      <Quote />
-      {/* Prevent reflections from auto-scrolling the window on Home */}
-      <Reflections autoScroll={false} />
       <Contact />
     </>
   );
